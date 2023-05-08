ccc399d1c3ff9f11abf1940c8693d031959e6250
Yonghyun Hwang <freeaion@gmail.com>
2023-05-08 15:18:24 -0700

    complete exercise for incorporating data section

--- checkbox/src/App.js
+++ checkbox/src/App.js
@@ -1,28 +1,97 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useMemo, useEffect, useState } from "react";
+import { FixedSizeList } from "react-window";
 
-function GitHubUser({ login }) {
-    const [data, setData] = useState();
-    const [error, setError] = useState();
-    const [loading, setLoading] = useState(false);
+const useIterator = (
+    items = [],
+    initIndex = 0
+) => {
+    const [i, setIndex] = useState(initIndex);
+
+    const prev = useCallback(() => {
+        setIndex(i === 0 ? items.length - 1 : i - 1);
+    }, [i]);
+    const next = useCallback(() => {
+        setIndex(i === items.length - 1 ? 0 : i + 1);
+    }, [i]);
+
+    const item = useMemo(() => items[i], [i]);
+    return [item, prev, next];
+}
+
+function RepoMenu({
+    repositories,
+    onSelect = f => f
+}) {
+    const [{ name }, prev, next] = useIterator(
+        repositories
+    );
 
     useEffect(() => {
-        if (!login) return;
-        setLoading(() => true);
-        console.log(`${login} is still hungry`);
-        fetch(`https://api.github.com/users/${login}`)
-            .then(resp => resp.json())
+        if (!name) return;
+        onSelect(name);
+    }, [name]);
+
+    return (
+        <div style={{ display: "flex" }}>
+            <button onClick={prev}>&lt;</button>
+            <p>{name}</p>
+            <button onClick={next}>&gt;</button>
+        </div>
+    );
+}
+
+function useFetch(uri) {
+    const [data, setData] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (!uri) return;
+        setLoading(true);
+        fetch(uri)
+            .then(data => data.json())
             .then(setData)
-            .then(() => setLoading(() => false))
+            .then(() => setLoading(false))
             .catch(setError);
-    }, [login]);
+    }, [uri]);
 
-    if (loading) {
-        return <h1> loading... </h1>;
-    }
-    if (error)
-        return <pre>{JSON.stringify(error, null, 2)}</pre>;
-    if (!data) return null;
+    return { loading, data, error };
+}
 
+function Fetch({
+    uri,
+    renderSuccess,
+    loadingFallback = <p> loading...</p>,
+    renderError = error => (
+        <pre>{JSON.stringify(error, null, 2)}</pre>
+    )
+}) {
+    const { loading, data, error } = useFetch(uri);
+    if (loading) return loadingFallback;
+    if (error) return renderError(error);
+    if (data) return renderSuccess({ data });
+
+    // nothing to render
+}
+
+function UserRepositories({
+    login,
+    onSelect = f => f
+}) {
+    return (
+        <Fetch
+            uri={`https://api.github.com/users/${login}/repos`}
+            renderSuccess={({ data }) => (
+                <RepoMenu
+                    repositories={data}
+                    onSelect={onSelect}
+                />
+            )}
+        />
+    );
+}
+
+function UserDetails({ data }) {
     return (
         <div className="githubUser">
             <img
@@ -35,37 +104,80 @@ function GitHubUser({ login }) {
                 {data.name && <p>{data.name}</p>}
                 {data.location && <p>{data.location}</p>}
             </div>
+            <UserRepositories
+                login={data.login}
+                onSelect={
+                    repoName => console.log(`${repoName}!!`)}
+            />
         </div>
     );
 }
 
-const tahoe_peaks = [
-    { name: "Freel Peak", elevation: 10891 },
-    { name: "Monument Peak", elevation: 10051 },
-    { name: "Pyramid Peak", elevation: 891 }
-];
-
-function List({ data = [], renderItem, renderEmpty }) {
-    if (!data.length) return renderEmpty;
+function GitHubUser({ login }) {
     return (
-        <ul>
-            {data.map((item, i) => (
-                <li key={i}>{renderItem(item)}</li>
-            ))}
-        </ul>
+        Fetch({
+            uri: `https://api.github.com/users/${login}`,
+            renderSuccess: UserDetails,
+        })
     );
 }
 
 export default function App() {
-    return (
-        <List
-            data={tahoe_peaks}
-            renderEmpty={<p>This list is empty</p>}
-            renderItem={item => (
-                <>
-                    {item.name} - {item.elevation.toLocaleString()}ft
-                </>
-            )}
-        />
-    );
+    const [login, setLogin] = useState("moontahoe");
+
+    return <GitHubUser login={login} />;
 }
+
+// const tahoe_peaks = [
+//     { name: "Freel Peak", elevation: 10891 },
+//     { name: "Monument Peak", elevation: 10051 },
+//     { name: "Pyramid Peak", elevation: 891 }
+// ];
+
+// function List({
+//     data = [],
+//     renderItem,
+//     renderEmpty = (<p> This list is empty</p>)
+// }) {
+//     if (!data.length) return renderEmpty;
+//     return (
+//         <ul>
+//             {data.map((item, i) => (
+//                 <li key={i}>{renderItem(item)}</li>
+//             ))}
+//         </ul>
+//     );
+// }
+
+
+// const bigList = [...Array(5000)].map(() => ({
+//     name: faker.name.findName(),
+//     email: faker.internet.email(),
+//     avatar: faker.internet.avatar()
+// }));
+
+// export default function App() {
+//     const renderRow = ({ index, style }) => (
+//         <div style={{ ...style, ...{ display: "flex" } }}>
+//             <img
+//                 src={bigList[index].avatar}
+//                 alt={bigList[index].name}
+//                 width={50}
+//             />
+//             <p>
+//                 {bigList[index].name} - {bigList[index].email}
+//             </p>
+//         </div>
+//     );
+
+//     return (
+//         <FixedSizeList
+//             height={window.innerHeight}
+//             width={window.innerWidth - 20}
+//             itemCount={bigList.length}
+//             itemSize={50}
+//         >
+//             {renderRow}
+//         </FixedSizeList>
+//     );
+// }
